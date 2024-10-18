ed84eb7ec6aaad9642dcbca5e8af6f3dcf7f4bc4
nizam-786mohaideen <nmohaideen98@gmail.com>
2024-10-18 23:33:58 +0530

    Update Insertion_Sort.java
    solved

--- java/Insertion_Sort.java
+++ java/Insertion_Sort.java
@@ -1,14 +1,18 @@
 public class Insertion_Sort {
     public int[] sortArray(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int key = nums[i];
+        // Loop through the array starting from the second element
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i]; // The element to be inserted
             int j = i - 1;
+
+            // Move elements that are greater than key to one position ahead
             while (j >= 0 && nums[j] > key) {
                 nums[j + 1] = nums[j];
                 j--;
             }
+            // Insert the key at the correct position
             nums[j + 1] = key;
         }
-        return nums;
+        return nums; // Return the sorted array
     }
 }
